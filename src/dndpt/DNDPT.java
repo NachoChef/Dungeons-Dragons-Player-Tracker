1aa1e11026b127455a0e93d86b0c2a502a5a9893
NachoChef <hock3ydud3@gmail.com>
2017-02-07 17:37:06 -0600

    Initial Commit

---
+++ src/dndpt/DNDPT.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dndpt;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Justin
+ */
+
+public class DNDPT {
+    
+    public static void connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:dnd35.db";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Database connected.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        connect();
+        String sql = "SELECT name FROM class";
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("name") + "\t" +
+                                   rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
